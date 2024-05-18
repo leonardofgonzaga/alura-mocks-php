100a0d1f011d534d86cbf104a0d4bbdc6fab3b13
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2024-05-18 12:39:24 -0300

    Aula 2: conhecendo mocks e stubs

--- tests/Service/EncerradorTest.php
+++ tests/Service/EncerradorTest.php
@@ -7,35 +7,36 @@ use DateTimeImmutable;
 use Alura\Leilao\Model\Leilao;
 use PHPUnit\Framework\TestCase;
 use Alura\Leilao\Service\Encerrador;
+use PDO;
 
-class LeilaoDaoMock extends LeilaoDao 
-{
-    private $leiloes = [];
+// class LeilaoDaoMock extends LeilaoDao 
+// {
+//     private $leiloes = [];
 
-    public function salva(Leilao $leilao): void
-    {
-        $this->leiloes[] = $leilao;
-    }
+//     public function salva(Leilao $leilao): void
+//     {
+//         $this->leiloes[] = $leilao;
+//     }
 
-    public function recuperarNaoFinalizados(): array
-    {
-        return array_filter($this->leiloes, function(Leilao $leilao) {
-            return !$leilao->estaFinalizado();
-        });
-    }
+//     public function recuperarNaoFinalizados(): array
+//     {
+//         return array_filter($this->leiloes, function(Leilao $leilao) {
+//             return !$leilao->estaFinalizado();
+//         });
+//     }
 
-    public function recuperarFinalizados(): array
-    {
-        return array_filter($this->leiloes, function(Leilao $leilao) {
-            return $leilao->estaFinalizado();
-        });
-    }
+//     public function recuperarFinalizados(): array
+//     {
+//         return array_filter($this->leiloes, function(Leilao $leilao) {
+//             return $leilao->estaFinalizado();
+//         });
+//     }
 
-    public function atualiza(Leilao $leilao): void
-    {
+//     public function atualiza(Leilao $leilao): void
+//     {
 
-    }
-}
+//     }
+// }
 
 class EncerradorTest extends TestCase
 {
@@ -51,22 +52,28 @@ class EncerradorTest extends TestCase
             new DateTimeImmutable('10 years ago')
         );
 
-        $leilaoDao = new LeilaoDaoMock();
-        $leilaoDao->salva($fiat147);
-        $leilaoDao->salva($variant);
+        $leilaoDao = $this->createMock(LeilaoDao::class);
+
+        // $leilaoDao = $this->getMockBuilder(LeilaoDao::class)
+        //     ->disableOriginalConstructor()
+        //     ->setConstructorArgs([new PDO('sqlite::memory:')])
+        //     ->getMock();
+
+        $leilaoDao->method('recuperarNaoFinalizados')
+            ->willReturn([$fiat147, $variant]);
+        $leilaoDao->expects($this->exactly(2))
+            ->method('atualiza')
+            ->withConsecutive(
+                [$fiat147],
+                [$variant]
+            );
 
         $encerrador = new Encerrador($leilaoDao);
         $encerrador->encerra();
 
-        $leiloes = $leilaoDao->recuperarFinalizados();
+        $leiloes = [$fiat147, $variant];
         self::assertCount(2, $leiloes);
-        self::assertEquals(
-            'Fiat 147 0Km',
-            $leiloes[0]->recuperarDescricao()
-        );
-        self::assertEquals(
-            'Variant 1972 0Km',
-            $leiloes[1]->recuperarDescricao()
-        );
+        self::assertTrue($leiloes[0]->estaFinalizado());
+        self::assertTrue($leiloes[1]->estaFinalizado());
     }
 }
